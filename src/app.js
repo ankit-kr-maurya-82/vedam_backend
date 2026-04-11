520659526e835852fadff600e4b4cdde2f156226
AK Maurya <ankitkrmaurya82@gmail.com>
2026-04-11 19:39:21 +0530

    url fx

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://vedam-fronted.vercel.app",  // allow requests from this origin
     credentials: true,               // allow cookies
 
 }))
