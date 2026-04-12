c3ffb21b9311f980e03e4061acf943b925e0daf6
AK Maurya <ankitkrmaurya82@gmail.com>
2026-04-12 20:11:07 +0530

    redis connect

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
 import dotenv from "dotenv";
 
 dotenv.config();
+
 import connectMongo from "./db/index.js";
 import { sqlDB } from "./db/sql.js";
 import { app } from "./app.js";
-
+import redis from "./db/redis.js";
 
 const startServer = async () => {
   try {
-    // ✅ MongoDB connect
+    // ✅ MongoDB
     await connectMongo();
 
-    // ✅ MySQL connect (test)
+    // ✅ MySQL
     const connection = await sqlDB.getConnection();
     console.log("✅ MySQL connected");
     connection.release();
 
-    // ✅ Start server
+    // ✅ Redis TEST (yahan hona chahiye)
+    await redis.set("test", "hello");
+    const data = await redis.get("test");
+    console.log("✅ Redis test:", data);
+
+    // ✅ Server start
     app.listen(process.env.PORT || 8000, () => {
       console.log(`🚀 Server running at port : ${process.env.PORT || 8000}`);
     });
@@ -26,5 +32,7 @@ const startServer = async () => {
     process.exit(1);
   }
 };
+
 console.log("ENV:", process.env.DB_USER);
+
 startServer();
