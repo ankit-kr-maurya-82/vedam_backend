1d4214e5a444d520c55b10426068994e367eac51
AK Maurya <ankitkrmaurya82@gmail.com>
2026-04-19 20:44:28 +0530

    bug fix into vercel deploy

--- src/app.js
+++ src/app.js
@@ -37,8 +37,8 @@ app.use(
    ✅ MIDDLEWARES
 ========================== */
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '25mb' }));
+app.use(express.urlencoded({ extended: true, limit: '25mb' }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
