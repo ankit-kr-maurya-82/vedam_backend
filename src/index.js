1d0b5d0a09650b0aa22054c05baa4dc617afd8e8
AK Maurya <ankitkrmaurya82@gmail.com>
2026-04-19 21:54:53 +0530

    bug

--- src/index.js
+++ src/index.js
@@ -1,34 +1,3 @@
-import "dotenv/config";
-import connectMongo from "./db/index.js";
 import { app } from "./app.js";
 
-// For Vercel Serverless (no listen)
-export default async function handler(req, res) {
-  try {
-    await connectMongo();
-    return app(req, res);
-  } catch (error) {
-    console.error("Handler error:", error);
-    res.status(500).json({ success: false, message: "Server error" });
-  }
-}
-
-// For local dev
-if (require.main === module) {
-  const startServer = async () => {
-    try {
-      await connectMongo();
-      console.log("✅ MongoDB connected");
-
-      const PORT = process.env.PORT || 8000;
-      app.listen(PORT, () => {
-        console.log(`🚀 Server running at port: ${PORT}`);
-      });
-    } catch (err) {
-      console.error("❌ Server start failed:", err.message);
-      process.exit(1);
-    }
-  };
-  startServer();
-}
-
+export default app;
