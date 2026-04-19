f55c91055d44df0b7dd62034ffba2d2e5d60c9da
AK Maurya <ankitkrmaurya82@gmail.com>
2026-04-19 22:15:51 +0530

    bug fix

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import "dotenv/config";
-import connectMongo from "./db/index.js";
 import app from "./app.js";
 
-// 🔥 Handle crashes
+// Handle errors for local
 process.on("uncaughtException", (err) => {
   console.error("❌ Uncaught Exception:", err);
   process.exit(1);
@@ -13,28 +12,12 @@ process.on("unhandledRejection", (err) => {
   process.exit(1);
 });
 
-const PORT = process.env.PORT || 8000;
-
-// Only start server if not Vercel (serverless)
-if (typeof process.env.VERCEL === "undefined") {
-  const startServer = async () => {
-    try {
-      // ✅ Connect MongoDB
-      await connectMongo();
-      console.log("✅ MongoDB connected");
-
-      // ✅ Start server
-      const server = app.listen(PORT, () => {
-        console.log(`🚀 Server running at port: ${PORT}`);
-      });
-
-    } catch (err) {
-      console.error("❌ Server start failed:", err.message);
-      process.exit(1);
-    }
-  };
-
-  startServer();
-}
-
 export default app;
+
+// Local server only
+if (typeof process.env.VERCEL === "undefined") {
+  const PORT = process.env.PORT || 8000;
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running at http://localhost:${PORT}`);
+  });
+}
